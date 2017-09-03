1400d0febf234c2515d117f9697db5c2e191ea45
selloutdesign <j.selloutdesign@gmail.com>
2017-09-03 13:59:26 -0700

    initial commit

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/meteor';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+    describe('Tasks', () => {
+        descibe('methods', () => {
+            it('can delete owned task', () => {
+
+            });
+        });
+    });
+}
